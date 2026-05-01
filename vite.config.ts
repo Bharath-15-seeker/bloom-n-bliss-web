392e20e9de47af4d19dc4b74fc8ed547ef5b0d3d
Bharath <bharathkandasamy15@gmail.com>
2026-05-01 11:46:46 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -8,3 +8,15 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
 });
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
